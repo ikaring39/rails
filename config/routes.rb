4a3105c5a08fce3cfae202f853bc7a80e3269795
ikaring39 <ikaring39@gmail.com>
2022-01-28 17:01:41 +0900

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get "home/register" => "home#register"  
   get "posts" => "posts#index"
   post "user_create" => "user#user_create"
-  post "user_delete/#{:id}" => "user#user_delete"
+  get "user_delete/:id" => "user#user_delete"
 
   # hello,world!を表示する
   # root 'application#hello'
