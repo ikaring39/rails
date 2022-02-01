12575fbba654717e88fc26ecd21ec86c097da644
ikaring39 <ikaring39@gmail.com>
2022-02-01 11:41:46 +0900

    登録成功/失敗時にメッセージを表示

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,17 @@
 class UserController < ApplicationController
-  # DBにデータを追加
+  # DBにデータを追加する
   def user_create
     @post = Post.create(:uuid => params[:uuid], :content => params[:content])
-    # flash[:success] = "ユーザー登録に成功しました！"
-    # flash[:alert] = "ユーザー登録に失敗しました…"
-    redirect_to("/posts")
+    # 登録に成功/失敗の結果を表示する
+    if @post.uuid.match(/\A[ｧ-ﾝﾞﾟ]+\z/) && @post.content.match(/\A[ｧ-ﾝﾞﾟ]+\z/)
+      flash[:success] = "ユーザー登録に成功しました！"
+      redirect_to("/posts")
+    else
+      flash[:alert] = "ユーザー登録に失敗しました…"
+      redirect_to("/posts")
+    end
   end
-  # DBからデータを削除
+  # DBからデータを削除する
   def user_destroy
     @post = Post.find(params[:id])
     @post.destroy
